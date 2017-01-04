43261cb9e706dd2f6a8e13f6a5a1c40cab5cb434
Todd Chaffee <tc@toddbiz.com>
2017-01-04 09:31:38 +0000

    Whoami works.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
-var app = express()
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+
+  var rtnObj = {};
+  
+  rtnObj.ipaddress = req.get('X-Forwarded-For');
+  rtnObj.language = req.get('ACCEPT-LANGUAGE').substr(0, req.get('ACCEPT-LANGUAGE').indexOf(','));
+  rtnObj.software = /\((.*?)\)/.exec(req.get('USER-AGENT'))[1];
+    
+  res.send(rtnObj);
 })
 
 app.listen(8080, function () {
